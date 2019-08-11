edc9edcec0ba878d77f5f3e6a906a71832e9916d
Ajay Sharma <ajsharm@microsoft.com>
2019-08-11 00:24:08 -0700

    Downloading actual files

--- download-files/async.py
+++ download-files/async.py
@@ -5,6 +5,7 @@ import time
 import asyncio
 from asyncio import AbstractEventLoop
 import aiohttp
+import aiofiles
 
 async def get_html(episode_number: int) -> str:
     print(f"Getting HTML for episode {episode_number}", flush=True)
@@ -16,26 +17,41 @@ async def get_html(episode_number: int) -> str:
             html = await res.text()
             return html
 
-def get_title(html: str, episode_number: int) -> str:
-    print(f"Getting TITLE for episode {episode_number}", flush=True)
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    header = soup.select_one('h1')
-    if not header:
-        return "NOTFOUND"
-    return header.text.strip()
+async def download_file(episode_number: str, aud_link: str):
+    print(f"Downloading audio for episode {episode_number}", flush=True)
+    f_name = f"{episode_number}-{aud_link.split('/')[-1]}"
+    url = f'https://talkpython.fm{aud_link}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as res:
+            res.raise_for_status()
+            f = await aiofiles.open(f_name, 'wb')
+            await f.write(await res.read())
+            await f.close()
+    print(f"Downloaded file {f_name}", flush=True)
 
-async def get_title_range(loop: AbstractEventLoop):
+def get_audiolink(html: str, episode_number: int) -> str:
+    print(f"Getting audio link for episode {episode_number}", flush=True)
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    audio = soup.select_one('source')
+    if not audio:
+        return "NOTFOUND"
+    return audio['src']
+
+async def get_audio_file_range(loop: AbstractEventLoop):
     tasks = []
+    dtasks = []
     for n in range(102, 120):
         tasks.append((loop.create_task(get_html(n)), n))
     for task, n in tasks:
         html = await task
-        title = get_title(html, n)
-        print(f"Title found: {title}", flush=True)
+        audlink = get_audiolink(html, n)
+        dtasks.append(loop.create_task(download_file(n, audlink)))
+    for task in dtasks:
+        await task
 
 def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_title_range(loop))
+    loop.run_until_complete(get_audio_file_range(loop))
     print("Done.")
 
 if __name__ == '__main__':
--- download-files/sync.py
+++ download-files/sync.py
@@ -9,22 +9,34 @@ def get_html(episode_number: int) -> str:
     res.raise_for_status()
     return res.text
 
-def get_title(html: str, episode_number: int) -> str:
-    print(f"Getting TITLE for episode {episode_number}", flush=True)
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    header = soup.select_one('h1')
-    if not header:
-        return "NOTFOUND"
-    return header.text.strip()
+def download_file(episode_number: str, aud_link: str):
+    print(f"Downloading audio for episode {episode_number}", flush=True)
+    f_name = f"{episode_number}-{aud_link.split('/')[-1]}"
+    url = f'https://talkpython.fm{aud_link}'
+    res = requests.get(url)
+    res.raise_for_status()
+    with open(f_name, 'wb') as f:
+        for chunk in res.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+    print(f"Downloaded file {f_name}", flush=True)
 
-def get_title_range():
+def get_audiolink(html: str, episode_number: int) -> str:
+    print(f"Getting audio link for episode {episode_number}", flush=True)
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    audio = soup.select_one('source')
+    if not audio:
+        return "NOTFOUND"
+    return audio['src']
+
+def get_audio_file_range():
     for n in range(102, 120):
         html = get_html(n)
-        title = get_title(html, n)
-        print(f"Title found: {title}", flush=True)
+        audlink = get_audiolink(html, n)
+        download_file(n, audlink)
 
 def main():
-    get_title_range()
+    get_audio_file_range()
     print("Done.")
 
 if __name__ == '__main__':
